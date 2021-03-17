bd3ac588c7d600280f589375af7377110f20dfc8
TSJ_小河 <ogawa@take-systems.jp>
2021-03-17 21:07:55 +0900

    [Updata]SQlite参照とJson参照を追加

--- Mail_main.py
+++ Mail_main.py
@@ -1,26 +1,40 @@
 # coding:utf-8
-import tkinter
+import tkinter 
 import json
 import sqlite3
-
+import os
 from tkinter import ttk
-class Main_form():
-    def __int__(self):
+class Main_form(object):
+    def __init__(self):
         self.dFile=""
-        try:
-            if os.path.isfile('Fileini.json'):
-                with open('Fileini.json') as f:
-                    
-                    dFile = json.load(f)
-                    dFile=dFile['Gray_Volt']
-                    #print(self.Label_1)
-        return
-if __name__ == "__main__":       
+        self.Mlist=[]
+        #try:
+        if os.path.isfile('Fileini.json'):
+            with open('Fileini.json') as f:
+                
+                dFile = json.load(f)
+                dFile=dFile['FlleLink']
+                conn = sqlite3.connect(dFile["SaveDB"])
+                cur = conn.cursor()
+                cur.execute("SELECT 機種名 FROM Model_table")
+                for row in (cur):
+                    self.Mlist.append(str(row[0]))
+                cur.close()
+                conn.close()
+                #print(self.Label_1)
+        #except:
+        #    return
+
+if __name__ == "__main__":   
+    Ma=Main_form()    
     main_win = tkinter.Tk()
+    TXRXLIST={'受信':0,'送信':1}
+    MODELLIST=Ma.Mlist
     main_win.title("メール選別")
     main_win.geometry("540x100")
     main_win.resizable(0,0)
-
+    v=tkinter.StringVar()
+    
     ## ウィジェット作成
     #--ラベル作成
     #機種名
@@ -28,14 +42,13 @@ if __name__ == "__main__":
     Model_label.place(x=10,y=10)
 
     #機種名入力（Combobox)
-    Model_List=ttk.Combobox()
+    Model_List=ttk.Combobox(main_win,values=MODELLIST)
     Model_List.place(x=60,y=10)
-
     #メール種類
     mail_label=tkinter.Label(text="種類:")
     mail_label.place(x=220,y=10)
     #機種名入力（Combobox)
-    mail_List=ttk.Combobox()
+    mail_List=ttk.Combobox(textvariable=v,values=list(TXRXLIST.keys()))
     mail_List.place(x=270,y=10)
 
     #Runボタン
