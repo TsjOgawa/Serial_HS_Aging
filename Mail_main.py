3cfa90dce2d2eb3c87e13c47afcad4837829e89e
tomoki4069 <t_simada@take-systems.jp>
2021-03-23 17:50:22 +0900

    1.機種名、メールの種類のコンボボックスの初期値設定 2.プログラム実行中はボタン操作を出来ないように変更 3.プログレスバーの追加 4.キャンセルボタンの実装

--- Mail_main.py
+++ Mail_main.py
@@ -40,6 +40,19 @@ class Main_form(object):
                     self.Mlist.append(str(row[0]))
                 cur_1.close()
                 conn_1.close()
+
+    def start(self):
+        global stop_flag
+        global thread
+
+        # スレッドが無いなら生成してstart()する
+        #if not thread:
+        thread = threading.Thread(target=self.delete,name="Thread_1")
+        thread.setDaemon(True)
+        stop_flag=False
+        thread.start()      
+
+
             
     def ask_folder(self):
         """　
@@ -48,19 +61,30 @@ class Main_form(object):
         #path = filedialog.askdirectory()
         #folder_path.set(path)
         #input_dir = folder_path.get()
+        Run_but_1.configure(state = "disabled")
+        Run_but_2.configure(state = "disabled")
+        Model_List.configure(state = "disabled")
+        mail_List.configure(state = "disabled")
+        pb2.start(100)
         self.list_B=(os.path.join(self.conn_2,Model_List.get()))
+        
         if not os.path.isdir(self.list_B):
         #if not os.path.isdir(os.path.join(self.conn_2,self.Mlist[0])):#<=
         #    for a in self.Mlist:
             os.makedirs(self.list_B)
             #self.list_B=(os.path.join(self.conn_2,Model_List.get()))
         self.list_C=os.path.join(self.list_B,mail_List.get())
-        if not os.path.isdir(os.path.join(self.list_B,mail_List.get())):
+        
+        self.list_B=os.path.join(self.list_B,"提出資料")
+        if  not os.path.isdir(self.list_B):
+            os.makedirs(self.list_B)
+        self.list_C=os.path.join(self.list_B,mail_List.get())
+        if not os.path.isdir(self.list_C):
             #os.path.join(path,list_B[0])
             #for b in self.list_B:
             #for b in self.list_A:
                 #for c in self.list_B:
-            os.makedirs(os.path.join(self.list_B,mail_List.get()))
+            os.makedirs(self.list_C)
                     #os.makedirs(os.path.join(self.list_B[b],self.list_A[1]))
         #Mailcheck
         print(mail_List.get())
@@ -70,7 +94,16 @@ class Main_form(object):
         thread.setDaemon(True)
         thread.start()
         thread.join()
-        messagebox.showinfo('Complete', 'DMail check completed.')   
+        messagebox.showinfo('終了！', 'メールファイル完成致しました。')
+
+        Run_but_1.configure(state = "normal")
+        Run_but_2.configure(state = "normal")
+        Model_List.configure(state = "normal")
+        mail_List.configure(state = "normal")
+        pb2.stop()
+    
+    def delete(self):
+        main_win.destroy()
 
         
 if __name__ == '__main__':
@@ -92,19 +125,25 @@ if __name__ == '__main__':
     #機種名入力（Combobox)
     Model_List=ttk.Combobox(main_win,values=MODELLIST)
     Model_List.place(x=60,y=10)
+    Model_List.set(MODELLIST[0])
     #メール種類
     mail_label=tkinter.Label(text="種類:")
     mail_label.place(x=220,y=10)
     #機種名入力（Combobox)
     mail_List=ttk.Combobox(textvariable=v,values=list(TXRXLIST.keys()))
     mail_List.place(x=270,y=10)
+    mail_List.set(list(TXRXLIST.keys())[0])
 
     folder_path = tkinter.StringVar()
 
-    #Runボタン
-    Run_but=tkinter.Button(text="実行",command=Ma.ask_folder,width=10)
-    Run_but.place(x=450,y=10)
+    #プログレスバー
+    pb2 = ttk.Progressbar(main_win,maximum=10,value=0,length=200,mode='indeterminate')
+    pb2.place(x=60,y=50)
 
-    Run_but=tkinter.Button(text="Cancel",width=10)
-    Run_but.place(x=450,y=50)
+    #Runボタン
+    Run_but_1=tkinter.Button(text="実行",command=Ma.ask_folder,width=10)
+    Run_but_1.place(x=450,y=10)
+
+    Run_but_2=tkinter.Button(text="Cancel",command=Ma.delete,width=10)
+    Run_but_2.place(x=450,y=50)
     main_win.mainloop()
